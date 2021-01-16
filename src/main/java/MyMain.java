d711d860bc2a34d8c79217999514bacfc40d5fe0
Jeremy-Lu-APComp <21jlu@athenian.org>
2021-01-16 04:48:23 +0000

    Initial commit - #1

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,8 +2,12 @@ public class MyMain {
 
     // Returns the mean of the 2D array mat
     public static double mean(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+        for(int i = 0; i<mat.length; i++){
+            for(int j=0; j<mat[0].length;j++){
+                int final_sum = mat[i][j] + matt[i+1][j+1];
+            }
+        }
+        return final_sum/mat.length;
     }
 
     // Returns the median of the 2D array mat
@@ -21,6 +25,8 @@ public class MyMain {
 
 
     public static void main(String[] args) {
-        // Write some code here to test your methods!
+        int[][] mat_1 = {{1, 5, 3}, {1, 2, 4}};
+        int average = mean(mat_1);
+        System.out.println("Average = " + average); //not sure why this isn't working.
     }
 }
